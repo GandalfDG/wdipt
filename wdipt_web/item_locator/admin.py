eed92cd0b1abf54a5cd0f49a03ca0ffcd6415ade
Jack Case <jackacase@gmail.com>
2025-02-27 20:45:06 +0000

    Inlining storage places for room in admin

--- wdipt_web/item_locator/admin.py
+++ wdipt_web/item_locator/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import GeoPlace, Room, StoragePlace, Item
 
-admin.site.register((GeoPlace, Room, StoragePlace, Item))
-# Register your models here.
+admin.site.register((GeoPlace, StoragePlace, Item))
+
+class StoragePlaceInline(admin.TabularInline):
+    model = StoragePlace
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    
+    inlines = [
+        StoragePlaceInline
+    ]
