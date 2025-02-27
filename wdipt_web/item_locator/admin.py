b933034aff851abea222e55c83499ddf54153599
Jack Case <jackacase@gmail.com>
2025-02-27 20:19:58 +0000

    making some item_locator models

--- wdipt_web/item_locator/admin.py
+++ wdipt_web/item_locator/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import GeoPlace, Room, StoragePlace, Item
 
+admin.site.register((GeoPlace, Room, StoragePlace, Item))
 # Register your models here.
--- wdipt_web/item_locator/models.py
+++ wdipt_web/item_locator/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class CommonModel(models.Model):
+    
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        abstract = True
+
+class GeoPlace(CommonModel):
+    
+    class Meta:
+        verbose_name = "geographic location"
+    
+class Room(CommonModel):
+    geo_place = models.ForeignKey(to=GeoPlace, on_delete=models.SET_NULL, null=True)
+
+class StoragePlace(CommonModel):
+    room = models.ForeignKey(to=Room, on_delete=models.SET_NULL, null=True)
+    
+class Item(CommonModel):
+    place = models.ForeignKey(to=StoragePlace, on_delete=models.SET_NULL, null=True)
