01a1c8eae91d69e8f8c962a953bf03d0d5b24180
Jack Case <jackacase@gmail.com>
2025-03-11 20:10:51 +0000

    add detail view for GeoPlace model

--- wdipt_web/item_locator/urls.py
+++ wdipt_web/item_locator/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import StoragePlaceDetailView, StoragePlaceCreateView, RoomDetailView
+from .views import StoragePlaceDetailView, StoragePlaceCreateView, RoomDetailView, GeoPlaceDetailView
 
 urlpatterns = [
     path("storage/", StoragePlaceCreateView.as_view()),
     path("storage/<int:pk>", StoragePlaceDetailView.as_view()),
-    path("room/<int:pk>", RoomDetailView.as_view())
+    path("room/<int:pk>", RoomDetailView.as_view()),
+    path("place/<int:pk>", GeoPlaceDetailView.as_view())
 ]
--- wdipt_web/item_locator/views.py
+++ wdipt_web/item_locator/views.py
@@ -2,23 +2,34 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.detail import DetailView
 
-from .models import StoragePlace, Room, Item
+from .models import StoragePlace, Room, GeoPlace, Item
 
 # Create your views here.
+class GeoPlaceDetailView(DetailView):
+    """Present all of the rooms in a place"""
+    model = GeoPlace
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["rooms"] = self.object.room_set.all()
+        return context
+
 class RoomDetailView(DetailView):
+    """Present all of the storage places in a room"""
     model = Room
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["storage_places"] = self.get_object().storageplace_set.all()
+        context["storage_places"] = self.object.storageplace_set.all()
         return context
     
 class StoragePlaceDetailView(DetailView):
+    """Present all of the items in a storage place"""
     model = StoragePlace
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["items"] = self.get_object().item_set.all()
+        context["items"] = self.object.item_set.all()
         return context
 
 class StoragePlaceCreateView(CreateView):
