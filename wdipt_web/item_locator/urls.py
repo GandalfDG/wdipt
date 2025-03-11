988ead9585dc984324f1d3661f051a7be094be00
Jack Case <jackacase@gmail.com>
2025-03-11 20:00:32 +0000

    created simple views which give room and storage place details
    a room's view lists its storage places
    a storage place's view lists its items

--- wdipt_web/item_locator/urls.py
+++ wdipt_web/item_locator/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from .views import StoragePlaceDetailView, StoragePlaceCreateView, RoomDetailView
+
 urlpatterns = [
-    
+    path("storage/", StoragePlaceCreateView.as_view()),
+    path("storage/<int:pk>", StoragePlaceDetailView.as_view()),
+    path("room/<int:pk>", RoomDetailView.as_view())
 ]
--- wdipt_web/item_locator/views.py
+++ wdipt_web/item_locator/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.detail import DetailView
 
-from .models import StoragePlace
+from .models import StoragePlace, Room, Item
 
 # Create your views here.
-class StorageCatalogView(CreateView):
+class RoomDetailView(DetailView):
+    model = Room
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["storage_places"] = self.get_object().storageplace_set.all()
+        return context
+    
+class StoragePlaceDetailView(DetailView):
     model = StoragePlace
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["items"] = self.get_object().item_set.all()
+        return context
+
+class StoragePlaceCreateView(CreateView):
+    model = StoragePlace
+    fields = ["name", "description", "room"]
+    
+class StoragePlaceCatalogItemsView():
+    pass
