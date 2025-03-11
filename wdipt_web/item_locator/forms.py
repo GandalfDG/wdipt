a4f9ed23970d680725080b9a514caf3af8fd9218
Jack Case <jackacase@gmail.com>
2025-03-11 19:01:41 +0000

    starting to tinker with forms and views

---
+++ wdipt_web/item_locator/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Room, StoragePlace, Item
+
+class RoomForm(forms.ModelForm):
+    
+    class Meta:
+        model = Room
+        fields = ["name", "description", "geo_place"]
+
+class StoragePlaceForm(forms.ModelForm):
+    
+    class Meta:
+        model = StoragePlace
+        fields = ["name", "description", "room"]
+        
+class ItemForm(forms.ModelForm):
+    
+    class Meta:
+        model = Item
+        fields = ["name", "description", "place"]
--- wdipt_web/item_locator/views.py
+++ wdipt_web/item_locator/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView
+
+from .models import StoragePlace
 
 # Create your views here.
+class StorageCatalogView(CreateView):
+    model = StoragePlace
