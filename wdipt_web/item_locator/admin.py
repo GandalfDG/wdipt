754adeebf665274296c78d9f672120b5ac56fdf4
Jack Case <jackacase@gmail.com>
2025-08-13 20:20:28 +0000

    add inline display of items to storage place admin

--- wdipt_web/item_locator/admin.py
+++ wdipt_web/item_locator/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
 from .models import GeoPlace, Room, StoragePlace, Item
 
-admin.site.register((GeoPlace, StoragePlace, Item))
+admin.site.register((GeoPlace, Item))
 
 class StoragePlaceInline(admin.TabularInline):
     model = StoragePlace
+
+class ItemInline(admin.TabularInline):
+    model = Item
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
     
     inlines = [
         StoragePlaceInline
+    ]
+
+@admin.register(StoragePlace)
+class StorageAdmin(admin.ModelAdmin):
+
+    inlines = [
+        ItemInline
     ]
